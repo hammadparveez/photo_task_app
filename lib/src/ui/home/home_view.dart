3df85c0bab989176e400d29abd416e8bb51e1e26
hammadpervez <hammadpervez6@gmail.com>
2021-12-27 03:19:59 +0500

    adding image select options

--- lib/src/ui/home/home_view.dart
+++ lib/src/ui/home/home_view.dart
@@ -1,12 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
+  _onPhotoTap(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (_) {
+          return AlertDialog(
+            title: Text('Upload a Photo'),
+            actions: [
+              TextButton.icon(
+                  onPressed: () => Navigator.pop(context),
+                  icon: Icon(Icons.close),
+                  label: Text('Close')),
+            ],
+            content: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextButton.icon(
+                    onPressed: _onGallerySelect,
+                    icon: const Icon(Icons.photo_library_outlined),
+                    label: const Text('Upload from Gallery')),
+                TextButton.icon(
+                    onPressed: _onCameraSelect,
+                    icon: const Icon(Icons.camera_alt),
+                    label: const Text('Upload from Camera'))
+              ],
+            ),
+          );
+        });
+  }
+
+  _onDeleteTap(BuildContext context) {}
+
+  _onGallerySelect() {
+    final imagePicker = ImagePicker();
+    imagePicker.pickImage(source: ImageSource.gallery);
+  }
+
+  _onCameraSelect() {
+    final imagePicker = ImagePicker();
+    imagePicker.pickImage(source: ImageSource.camera);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('hi'),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+              onPressed: () => _onDeleteTap(context),
+              child: const Icon(Icons.delete)),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: () => _onPhotoTap(context),
+            child: const Icon(Icons.add_a_photo),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (_, index) {
+            return Text("Hi");
+          }),
     );
   }
 }
---
+++ lib/src/ui/photo_ui/photo_upload_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PhotoUploadView extends StatelessWidget {
+  const PhotoUploadView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
